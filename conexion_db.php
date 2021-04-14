83c428ce7aa5f191f72f41dbfb175e840b919259
lefrycarvajal <75230437+lefrycarvajal@users.noreply.github.com>
2021-04-14 00:23:00 -0400

    Update conexion_db.php

--- conexion_db.php
+++ conexion_db.php
@@ -1,5 +1,5 @@
 <?php
 
-$conex = mysqli_connect("localhost","root","","rdportfolio");
+$conex = mysqli_connect("grp6m5lz95d9exiz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","o1z78z0fet5bvkx2","h4mwqc052zjm4kx4","dq30nyby6vmip2t0");
 
 ?>
